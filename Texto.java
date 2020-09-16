bbfd90a7dda6c3e8b2f71c5681f8e2e9750a8fb6
Martin Verniere <martincho7verniere@yahoo.com>
2020-09-15 21:27:10 -0300

    Cuarto commit

--- Texto.java
+++ Texto.java
@@ -2,7 +2,7 @@
 public class Texto {
 		private String Txt;
 		public Texto() {
-			Txt="Prueba para TdeP";
+			Txt="Prueba para tener un conflicto para TdeP";
 		}
 		public String getTexto() {
 			return Txt;
